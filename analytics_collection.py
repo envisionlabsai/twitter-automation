498a88d2ad662980d6f61c622253a0632b845c55
Your Name <youremail@example.com>
2024-08-05 20:14:29 -0300

    Updated analytics collection with OAuth 1.0a

--- analytics_collection.py
+++ analytics_collection.py
@@ -1,8 +1,7 @@
-# analytics_collection.py
 import os
 import sqlite3
-import tweepy
 import requests
+from requests_oauthlib import OAuth1Session
 
 # Load environment variables
 TWITTER_API_KEY = os.getenv('TWITTER_API_KEY')
@@ -10,26 +9,27 @@ TWITTER_API_SECRET_KEY = os.getenv('TWITTER_API_SECRET_KEY')
 TWITTER_ACCESS_TOKEN = os.getenv('TWITTER_ACCESS_TOKEN')
 TWITTER_ACCESS_TOKEN_SECRET = os.getenv('TWITTER_ACCESS_TOKEN_SECRET')
 
-# Set up Twitter API client
-client = tweepy.Client(bearer_token=os.getenv('TWITTER_BEARER_TOKEN'),
-                       consumer_key=TWITTER_API_KEY,
-                       consumer_secret=TWITTER_API_SECRET_KEY,
-                       access_token=TWITTER_ACCESS_TOKEN,
-                       access_token_secret=TWITTER_ACCESS_TOKEN_SECRET)
+# Set up OAuth1 session
+oauth = OAuth1Session(
+    client_key=TWITTER_API_KEY,
+    client_secret=TWITTER_API_SECRET_KEY,
+    resource_owner_key=TWITTER_ACCESS_TOKEN,
+    resource_owner_secret=TWITTER_ACCESS_TOKEN_SECRET
+)
 
 def collect_tweet_data():
     conn = sqlite3.connect('analytics.db')
     cursor = conn.cursor()
     cursor.execute('''CREATE TABLE IF NOT EXISTS tweet_analytics
                       (tweet_id TEXT PRIMARY KEY, text TEXT, engagement INTEGER)''')
-    print("Table tweet_analytics created or already exists.")
     
-    tweets = client.get_users_tweets(id=client.get_me().data.id, max_results=100)
-    for tweet in tweets.data:
-        engagement = tweet.public_metrics['like_count'] + tweet.public_metrics['retweet_count']
+    response = oauth.get('https://api.twitter.com/2/tweets?ids=your_tweet_ids&tweet.fields=public_metrics,non_public_metrics,organic_metrics')
+    tweets = response.json()['data']
+    
+    for tweet in tweets:
+        engagement = tweet['public_metrics']['like_count'] + tweet['public_metrics']['retweet_count']
         cursor.execute("INSERT OR IGNORE INTO tweet_analytics (tweet_id, text, engagement) VALUES (?, ?, ?)",
-                       (tweet.id, tweet.text, engagement))
-        print(f"Inserted tweet ID {tweet.id} with engagement {engagement}")
+                       (tweet['id'], tweet['text'], engagement))
     
     conn.commit()
     conn.close()
