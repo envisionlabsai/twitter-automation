c0e554a5aee948c3d168e6490914d704fd6610e3
Your Name <youremail@example.com>
2024-08-05 20:16:02 -0300

    Added response debugging to analytics_collection.py

--- analytics_collection.py
+++ analytics_collection.py
@@ -23,17 +23,24 @@ def collect_tweet_data():
     cursor.execute('''CREATE TABLE IF NOT EXISTS tweet_analytics
                       (tweet_id TEXT PRIMARY KEY, text TEXT, engagement INTEGER)''')
     
-    response = oauth.get('https://api.twitter.com/2/tweets?ids=your_tweet_ids&tweet.fields=public_metrics,non_public_metrics,organic_metrics')
-    tweets = response.json()['data']
+    # Example tweet IDs, replace with actual IDs
+    tweet_ids = 'your_tweet_ids'  
+    url = f'https://api.twitter.com/2/tweets?ids={tweet_ids}&tweet.fields=public_metrics'
+    response = oauth.get(url)
     
-    for tweet in tweets:
-        engagement = tweet['public_metrics']['like_count'] + tweet['public_metrics']['retweet_count']
-        cursor.execute("INSERT OR IGNORE INTO tweet_analytics (tweet_id, text, engagement) VALUES (?, ?, ?)",
-                       (tweet['id'], tweet['text'], engagement))
-    
-    conn.commit()
+    print("Full Response:", response.json())  # Print the full response for debugging
+
+    if 'data' in response.json():
+        tweets = response.json()['data']
+        for tweet in tweets:
+            engagement = tweet['public_metrics']['like_count'] + tweet['public_metrics']['retweet_count']
+            cursor.execute("INSERT OR IGNORE INTO tweet_analytics (tweet_id, text, engagement) VALUES (?, ?, ?)",
+                           (tweet['id'], tweet['text'], engagement))
+        conn.commit()
+    else:
+        print("Error: 'data' key not found in the response.")
+
     conn.close()
 
 if __name__ == "__main__":
     collect_tweet_data()
-
