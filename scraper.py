427d1b6059fe55b5c3bdcd7d54862816e09f6fd4
Your Name <youremail@example.com>
2024-08-05 20:47:48 -0300

    Add scraper script and update Procfile

---
+++ scraper.py
@@ -0,0 +1,32 @@
+# scraper.py
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+import os
+
+def get_tweet_data(tweet_url):
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+
+    driver_service = Service(executable_path="/usr/bin/chromedriver")
+    driver = webdriver.Chrome(service=driver_service, options=chrome_options)
+    
+    driver.get(tweet_url)
+    page_source = driver.page_source
+    driver.quit()
+    
+    soup = BeautifulSoup(page_source, 'html.parser')
+    
+    try:
+        views = soup.find('div', {'data-testid': 'viewCount'}).get_text()
+        print(f'Views: {views}')
+    except AttributeError:
+        print('Views data not found.')
+
+if __name__ == "__main__":
+    tweet_url = 'https://twitter.com/your_tweet_url'  # Replace with the actual tweet URL
+    get_tweet_data(tweet_url)
