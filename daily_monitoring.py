6ab4b5237f8f6eb6660dc38bc5973fac790ec3bc
Your Name <youremail@example.com>
2024-08-05 18:13:19 -0300

    Updated script to test Telegram messaging and added requests to requirements

--- daily_monitoring.py
+++ daily_monitoring.py
@@ -1,44 +1,23 @@
-import sqlite3
-import json
-import tweepy
 import os
-from dotenv import load_dotenv
+import requests
 
-# Load environment variables from .env file
-load_dotenv()
+# Load environment variables
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-# Twitter API credentials
-API_KEY = os.getenv('TWITTER_API_KEY')
-API_SECRET_KEY = os.getenv('TWITTER_API_SECRET_KEY')
-ACCESS_TOKEN = os.getenv('TWITTER_ACCESS_TOKEN')
-ACCESS_TOKEN_SECRET = os.getenv('TWITTER_ACCESS_TOKEN_SECRET')
-BEARER_TOKEN = os.getenv('TWITTER_BEARER_TOKEN')
+def send_telegram_message(message):
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+    data = {
+        "chat_id": TELEGRAM_CHAT_ID,
+        "text": message
+    }
+    response = requests.post(url, data=data)
+    return response
 
-# Set up Twitter API client
-client = tweepy.Client(bearer_token=BEARER_TOKEN,
-                       consumer_key=API_KEY,
-                       consumer_secret=API_SECRET_KEY,
-                       access_token=ACCESS_TOKEN,
-                       access_token_secret=ACCESS_TOKEN_SECRET)
-
-def update_engagement_metrics():
-    conn = sqlite3.connect('tweets.db')
-    cursor = conn.cursor()
-    cursor.execute("SELECT tweet_id FROM tweets")
-    tweet_ids = cursor.fetchall()
-
-    for tweet_id in tweet_ids:
-        tweet_id = tweet_id[0]
-        tweet = client.get_tweet(tweet_id, tweet_fields=["public_metrics"])
-        engagement = tweet.data.public_metrics
-        cursor.execute("""
-            UPDATE tweets
-            SET engagement = ?
-            WHERE tweet_id = ?
-        """, (json.dumps(engagement), tweet_id))
-
-    conn.commit()
-    conn.close()
+def main():
+    message = "Test message from Heroku!"
+    response = send_telegram_message(message)
+    print(response.status_code, response.text)
 
 if __name__ == "__main__":
-    update_engagement_metrics()
+    main()
